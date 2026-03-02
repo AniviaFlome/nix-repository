eb0785993bdbdc3ffc84897d50bcdff4bb14875c
AniviaFlome <66728147+AniviaFlome@users.noreply.github.com>
2026-03-02 17:19:26 +0000

    pkgs: auto-update

--- pkgs/proton-cachyos/default.nix
+++ pkgs/proton-cachyos/default.nix
@@ -11,11 +11,11 @@
 }:
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "proton-cachyos";
-  version = "10.0-20260207";
+  version = "10.0-20260227";
 
   src = fetchzip {
     url = "https://github.com/CachyOS/proton-cachyos/releases/download/cachyos-${finalAttrs.version}-slr/proton-cachyos-${finalAttrs.version}-slr-x86_64${marchLevel}.tar.xz";
-    hash = "sha256-LWiW601Dy5BMDQqjU+FnEl70a+9SoVjsJDWcCEUNij8=";
+    hash = "sha256-cf8FNqE7jEnJ4SBRmPn5Qj01mRKssBHdAJ/XGmonPNk=";
   };
 
   dontUnpack = true;
