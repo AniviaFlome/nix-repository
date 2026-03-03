1f164d09ff7b992434a7a68e6223cb528eaaa9ee
AniviaFlome <aniviaflome@gmail.com>
2026-03-03 17:30:14 +0300

    some changes

--- pkgs/hyprism/default.nix
+++ pkgs/hyprism/default.nix
@@ -3,7 +3,7 @@
   stdenv,
   fetchurl,
   autoPatchelfHook,
-  makeWrapper,
+  buildFHSEnv,
   nix-update-script,
   gtk3,
   glib,
@@ -26,79 +26,98 @@
   openssl,
 }:
 
-stdenv.mkDerivation (finalAttrs: {
+let
+  hyprism-unwrapped = stdenv.mkDerivation (finalAttrs: {
+    pname = "hyprism-unwrapped";
+    version = "3.0.1";
+
+    src = fetchurl {
+      url = "https://github.com/hyprismteam/HyPrism/releases/download/v${finalAttrs.version}/HyPrism-linux-x64-${finalAttrs.version}.tar.xz";
+      hash = "sha256-uelinfGoJrvEvKKMP+jgZRU3sQln0Aup9oUqlLVVS2g=";
+    };
+
+    nativeBuildInputs = [
+      autoPatchelfHook
+    ];
+
+    preferLocalBuild = true;
+
+    dontStrip = true;
+
+    buildInputs = [
+      gtk3
+      glib
+      nss
+      nspr
+      alsa-lib
+      cups
+      libdrm
+      expat
+      libxkbcommon
+      mesa
+      libGL
+      pango
+      cairo
+      at-spi2-atk
+      gdk-pixbuf
+    ];
+
+    autoPatchelfIgnoreMissingDeps = [ "liblttng-ust.so.0" ];
+
+    installPhase = ''
+      runHook preInstall
+
+      mkdir -p $out/lib/hyprism
+
+      cp -r * $out/lib/hyprism/
+
+      runHook postInstall
+    '';
+
+    passthru.updateScript = nix-update-script { };
+
+    meta = {
+      description = "Hytale launcher with mod management";
+      homepage = "https://github.com/HyPrismTeam/HyPrism";
+      license = lib.licenses.gpl3Only;
+      platforms = [ "x86_64-linux" ];
+      maintainers = [ ];
+      sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
+    };
+  });
+in
+buildFHSEnv {
   pname = "hyprism";
-  version = "3.0.1";
+  inherit (hyprism-unwrapped) version meta;
 
-  src = fetchurl {
-    url = "https://github.com/hyprismteam/HyPrism/releases/download/v${finalAttrs.version}/HyPrism-linux-x64-${finalAttrs.version}.tar.xz";
-    hash = "sha256-uelinfGoJrvEvKKMP+jgZRU3sQln0Aup9oUqlLVVS2g=";
-  };
+  targetPkgs =
+    _:
+    [
+      hyprism-unwrapped
+      icu
+      openssl
+      libGL
+    ];
 
-  nativeBuildInputs = [
-    autoPatchelfHook
-    copyDesktopItems
-    makeWrapper
-  ];
+  runScript = "${hyprism-unwrapped}/lib/hyprism/HyPrism";
 
-  preferLocalBuild = true;
-
-  dontStrip = true;
-
-  buildInputs = [
-    gtk3
-    glib
-    nss
-    nspr
-    alsa-lib
-    cups
-    libdrm
-    expat
-    libxkbcommon
-    mesa
-    libGL
-    pango
-    cairo
-    at-spi2-atk
-    gdk-pixbuf
-  ];
-
-  autoPatchelfIgnoreMissingDeps = [ "liblttng-ust.so.0" ];
-
-  desktopItems = [
-    (makeDesktopItem {
-      name = "hyprism";
-      exec = "hyprism";
-      icon = "hyprism";
-      comment = "Hytale launcher with mod management";
-      desktopName = "HyPrism";
-      categories = [ "Game" ];
-    })
-  ];
-
-  installPhase = ''
-    runHook preInstall
-
-    mkdir -p $out/bin $out/lib/hyprism
-
-    cp -r * $out/lib/hyprism/
-
-    ln -s $out/lib/hyprism/HyPrism $out/bin/hyprism
-    wrapProgram $out/bin/hyprism \
-      --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath [ libGL icu openssl ]}
-
-    runHook postInstall
-  '';
+  extraInstallCommands =
+    let
+      desktopItems = [
+        (makeDesktopItem {
+          name = "hyprism";
+          exec = "hyprism";
+          icon = "hyprism";
+          comment = "Hytale launcher with mod management";
+          desktopName = "HyPrism";
+          categories = [ "Game" ];
+        })
+      ];
+    in
+    ''
+      ${lib.concatMapStringsSep "\n" (item: "cp -r ${item}/share $out/") desktopItems}
+      install -Dm644 ${./icon.png} $out/share/icons/hicolor/800x800/apps/hyprism.png
+    '';
 
   passthru.updateScript = nix-update-script { };
-
-  meta = {
-    description = "Hytale launcher with mod management";
-    homepage = "https://github.com/HyPrismTeam/HyPrism";
-    license = lib.licenses.gpl3Only;
-    platforms = [ "x86_64-linux" ];
-    maintainers = [ ];
-    sourceProvenance = [ lib.sourceTypes.binaryNativeCode ];
-    mainProgram = "hyprism";
-  };
-})
+}
