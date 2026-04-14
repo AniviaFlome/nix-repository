acb77d4fa3990b9a592d3a83851c773cd6d47411
AniviaFlome <66728147+AniviaFlome@users.noreply.github.com>
2026-04-14 02:14:08 +0000

    pkgs: auto-update

--- pkgs/motrix-next-bin/default.nix
+++ pkgs/motrix-next-bin/default.nix
@@ -8,13 +8,13 @@
 
 let
   pname = "motrix-next-bin";
-  version = "3.6.9";
+  version = "3.6.10";
 
   arch = if stdenv.hostPlatform.isAarch64 then "aarch64" else "amd64";
 
   src = fetchurl {
     url = "https://github.com/AnInsomniacy/motrix-next/releases/download/v${version}/MotrixNext_${version}_${arch}.AppImage";
-    hash = "sha256-Xsae42thbUXNCiefTuZz2NS9epU1YC/X4sPVV9/jln8=";
+    hash = "sha256-0lq0bP8bV1UT8dTjlNvhQ5pBsTqIbX5ryhfaWQ/DkOs=";
   };
 in
 appimageTools.wrapType2 {
