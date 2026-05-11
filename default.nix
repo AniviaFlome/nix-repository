e408d50831fb066cf940d2735a5b69f0774f862b
aniviaflome </home/aniviaflome/.config/sops-nix/secrets/mail>
2026-05-11 23:53:34 +0300

    changes

--- default.nix
+++ default.nix
@@ -36,8 +36,6 @@ in
   crankshaft-bin = pkgs.callPackage ./pkgs/crankshaft-bin { };
   motrix-next-bin = pkgs.callPackage ./pkgs/motrix-next-bin { };
   bloomeetunes-bin = pkgs.callPackage ./pkgs/bloomeetunes-bin { };
-  wizard101 = pkgs.callPackage ./pkgs/wizard101 { };
-  crystal-realms = pkgs.callPackage ./pkgs/crystal-realms { };
 
   mpvScripts =
     let
