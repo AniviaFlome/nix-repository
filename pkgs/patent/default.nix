37af219f310b68e7e102d0c23117788b951cbd9f
aniviaflome </home/aniviaflome/.config/sops-nix/secrets/mail>
2026-06-26 23:48:31 +0300

    changes

--- pkgs/patent/default.nix
+++ pkgs/patent/default.nix
@@ -1,60 +1,61 @@
 {
   lib,
-  stdenv,
-  fetchurl,
-  autoPatchelfHook,
+  rustPlatform,
+  fetchFromGitHub,
   openssl,
+  pkg-config,
+  onnxruntime,
+  makeWrapper,
   nix-update-script,
 }:
 
-let
-  hashes = {
-    x86_64-linux = "sha256-mU8umYWl0oI34rRh4YwZaLGjqEAVpqJbXAXntwhpVu0=";
-    aarch64-linux = "sha256-yj+aCMYHB8BaQytmWfvx82tRWm6zKxU9ed5Be/UwLng=";
-  };
-
-  arch =
-    {
-      x86_64-linux = "x86_64";
-      aarch64-linux = "aarch64";
-    }
-    .${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
-in
-stdenv.mkDerivation (finalAttrs: {
+rustPlatform.buildRustPackage rec {
   pname = "patent";
   version = "0.5.1";
 
-  src = fetchurl {
-    url = "https://github.com/r14dd/patent/releases/download/v${finalAttrs.version}/patent-${arch}-unknown-linux-gnu.tar.xz";
-    hash =
-      hashes.${stdenv.hostPlatform.system} or (throw "Unsupported system: ${stdenv.hostPlatform.system}");
+  src = fetchFromGitHub {
+    owner = "r14dd";
+    repo = "patent";
+    rev = "v${version}";
+    hash = "sha256-pkgC4z25Jdh7Erex0UJp/tM3t/QdZyp7YOcjB5qVcpM=";
   };
 
-  sourceRoot = ".";
+  # ponytail: fastembed default pulls ort download-binaries (fetches ONNX runtime at build time, breaks sandbox).
+  # Switch to load-dynamic: dlopen libonnxruntime at runtime via ORT_DYLIB_PATH.
+  postPatch = ''
+    substituteInPlace Cargo.toml --replace \
+      'fastembed = "5"' \
+      'fastembed = { version = "5", default-features = false, features = [ "ort-load-dynamic", "hf-hub-native-tls", "image-models" ] }'
+    cp ${./Cargo.lock} Cargo.lock
+  '';
 
-  nativeBuildInputs = [ autoPatchelfHook ];
+  cargoLock = {
+    lockFile = ./Cargo.lock;
+    allowBuiltinFetchGit = true;
+  };
 
-  buildInputs = [
-    openssl
-    stdenv.cc.cc.lib
+  nativeBuildInputs = [
+    pkg-config
+    makeWrapper
   ];
 
-  installPhase = ''
-    install -Dm755 patent-*/patent $out/bin/patent
+  buildInputs = [ openssl ];
+
+  # ponytail: rank tests download embedding model from HF, needs network at build time.
+  doCheck = false;
+
+  postInstall = ''
+    wrapProgram $out/bin/patent --set ORT_DYLIB_PATH ${lib.getLib onnxruntime}/lib/libonnxruntime.so
   '';
 
   passthru.updateScript = nix-update-script { };
 
-  meta = with lib; {
+  meta = {
     description = "Prior-art search for your code ideas";
     homepage = "https://github.com/r14dd/patent";
-    license = licenses.mit;
+    license = lib.licenses.mit;
     maintainers = [ ];
     mainProgram = "patent";
-    platforms = [
-      "x86_64-linux"
-      "aarch64-linux"
-    ];
-    sourceProvenance = [ sourceTypes.binaryNativeCode ];
+    platforms = lib.platforms.linux;
   };
-})
+}
