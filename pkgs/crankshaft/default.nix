855713b04cc21cb635b8c01b73d91a5c93f68882
aniviaflome </home/aniviaflome/.config/sops-nix/secrets/mail>
2026-08-04 17:20:14 +0300

    changes

--- pkgs/crankshaft/default.nix
+++ pkgs/crankshaft/default.nix
@@ -10,8 +10,8 @@ let
   version = "2.0.1";
 
   src = fetchurl {
-    url = "https://github.com/KraXen72/crankshaft/releases/download/${version}/crankshaft-portable-linux-x86_64.AppImage";
-    sha256 = "sha256-sLX0v1WOotHmnA4TPN8JKtE6xoQqqbKBai9/BYBmwgY=";
+    url = "https://github.com/KraXen72/crankshaft/releases/download/${version}/crankshaft-x64.AppImage";
+    hash = "sha256-BoqicNpINhWecnP6eIOZE3j6p2xdy+J8CTEHG2CfptE=";
   };
 
   appimageContents = appimageTools.extractType2 {
@@ -23,9 +23,7 @@ appimageTools.wrapType2 {
 
   extraInstallCommands = ''
     install -Dm444 ${appimageContents}/crankshaft.desktop -t $out/share/applications/
-    install -Dm444 ${appimageContents}/usr/share/icons/hicolor/0x0/apps/crankshaft.png $out/share/icons/hicolor/512x512/apps/crankshaft.png
-    substituteInPlace $out/share/applications/crankshaft.desktop \
-      --replace-warn 'Exec=AppRun' 'Exec=crankshaft'
+    install -Dm444 ${appimageContents}/usr/share/icons/hicolor/512x512/apps/crankshaft.png $out/share/icons/hicolor/512x512/apps/crankshaft.png
   '';
 
   passthru.updateScript = nix-update-script { };
