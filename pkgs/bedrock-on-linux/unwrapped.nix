a65e4519f64ff394f919180b0779c093386f300a
AniviaFlome <66728147+AniviaFlome@users.noreply.github.com>
2026-08-12 01:31:42 +0000

    pkgs: auto-update

--- pkgs/bedrock-on-linux/unwrapped.nix
+++ pkgs/bedrock-on-linux/unwrapped.nix
@@ -20,11 +20,11 @@ let
 in
 stdenvNoCC.mkDerivation (finalAttrs: {
   pname = "bedrock-on-linux-unwrapped";
-  version = "2.1.3";
+  version = "2.1.4";
 
   src = fetchurl {
     url = "https://github.com/Wyze3306/BedrockOnLinux/releases/download/v${finalAttrs.version}/bedrock-on-linux_${finalAttrs.version}_amd64.deb";
-    hash = "sha256-i3rXA+/Te+I6lW9QJ1gwUZRpZykqiLVniPFkA2KU97g=";
+    hash = "sha256-9/GBtJL9IUgy64mIrvsJvT3nibD/CmWt4POUjSsvYVw=";
   };
 
   nativeBuildInputs = [
